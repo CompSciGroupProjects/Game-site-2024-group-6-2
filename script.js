1b12bda98ed3667e4f9cbbb2e4655ad4d53683ec
j4redj <113187532+j4redj@users.noreply.github.com>
2024-06-04 12:37:47 -0400

    Finished main page

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirectRPS() {
+    window.location.href = 'RockPaperScissors';
+}
+
+function redirectTTT() {
+    window.location.href = 'TicTacToes';
+}
+
+function redirectC4() {
+    window.location.href = 'Connect4';
+}
+
+function redirectHM() {
+    window.location.href = 'Hangman';
+}
